0aab979d42eaca094ada1efd12259142ca4aa71f
DamianDomzz <dominguezdamian35@yahoo.com.ar>
2019-12-15 19:38:42 -0300

    console log random

--- front-end/nombre.js
+++ front-end/nombre.js
@@ -5,6 +5,7 @@ function traer_nombre() {
 
     NombreUsuario = product = document.getElementsByName("nombre")[0].value;
  console.log(NombreUsuario)  
+ console.log("dasdaad")
 }
  
 traer_nombre()
